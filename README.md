b894658b1ec7087b23e36578aafbf36684b09af7
root <fernando.bac03@hotmail.com>
2017-05-30 16:17:44 +0200

    creating new extension for ckan - predataset extension

---
+++ README.md
@@ -0,0 +1,2 @@
+# ckanext-predataset
+This extension allows us to select a data set that will be the basis for creating a new dataset
---
+++ ckanext/predataset/plugin.py
@@ -0,0 +1,31 @@
+import ckan.plugins as p
+
+
+class Pre_AddDatasetPlugin(p.SingletonPlugin):
+    '''Selection of a dataset prior to the creation of a new dataset with autocompleted fields'''
+
+    p.implements(p.IRoutes,inherit=True)
+    p.implements(p.IConfigurer)
+
+    def before_map(self, map):
+ 	
+	map.connect('add dataset new form', '/dataset/new_form',controller='ckanext.predataset.controller:DDController', action='schema_select')    
+
+	map.connect('add dataset', '/dataset/new_dataset',controller='ckanext.predataset.controller:DDController', action='new_dataset')          
+ 	
+        return map
+
+    def after_map(self, map):
+	
+	map.connect('add dataset new form', '/dataset/new_form',controller='ckanext.predataset.controller:DDController', action='schema_select')       
+	
+	map.connect('add dataset', '/dataset/new_dataset',controller='ckanext.predataset.controller:DDController', action='new_dataset')   
+
+        return map
+
+
+
+    def update_config(self, config_):
+        p.toolkit.add_template_directory(config_, 'templates')
+        p.toolkit.add_public_directory(config_, 'public')
+        p.toolkit.add_resource('fanstatic', 'dictionary')
